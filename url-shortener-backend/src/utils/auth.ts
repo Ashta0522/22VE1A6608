e6cd8c4d13652e0403883d899272b17eea135cdc
Rama Ashta Lakshmi Chakilam <145862099+Ashta0522@users.noreply.github.com>
2025-07-18 13:36:12 +0530

    Update auth.ts

--- url-shortener-backend/src/utils/auth.ts
+++ url-shortener-backend/src/utils/auth.ts
@@ -7,7 +7,7 @@ const REGISTER_API = "http://20.244.56.144/evaluation-service/register";
 const AUTH_API = "http://20.244.56.144/evaluation-service/auth";
 
 const credentials = {
-  companyName: "", // Optional, fill if required
+  companyName: "", 
   clientID: "c504cee7-e893-4258-8501-217dfb043c93",
   clientSecret: "euHBSjftNsxPuESn",
   ownerName: "Rama Ashta Lakshmi Chakilam",
@@ -20,10 +20,10 @@ export async function getAuthToken(): Promise<string> {
   const now = Date.now();
   if (cachedToken && now < tokenExpiry) return cachedToken;
 
-  // Register (ignore errors if already registered)
+  
   await axios.post(REGISTER_API, credentials).catch(() => {});
 
-  // Authenticate and get token
+  
   const response = await axios.post(AUTH_API, {
     email: credentials.ownerEmail,
     name: credentials.ownerName,
